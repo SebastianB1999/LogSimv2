dc733973666ba1de563e22b493f605bcd639b6ed
SebastianB1999 <34735935+SebastianB1999@users.noreply.github.com>
2018-04-24 13:24:29 +0200

    v13

--- LogSim/LogfuncTest.py
+++ LogSim/LogfuncTest.py
@@ -67,32 +67,61 @@ class OrGateTest(unittest.TestCase):
 
 class NandGateTest(unittest.TestCase):
     def testcase_01(self):
-        a = OrGate()
+        a = NandGate()
+        a.Input0 = False
+        a.Input1 = False
+        a.execute()
+        self.assertEqual(True, a.Output, "Class OrGate Testcase 1 failed.")
+
+    def testcase_02(self):
+        a = NandGate()
+        a.Input0 = False
+        a.Input1 = True
+        a.execute()
+        self.assertEqual(False, a.Output, "Class OrGate Testcase 2 failed.")
+
+    def testcase_03(self):
+        a = NandGate()
+        a.Input0 = True
+        a.Input1 = False
+        a.execute()
+        self.assertEqual(False, a.Output, "Class OrGate Testcase 3 failed.")
+
+    def testcase_04(self):
+        a = NandGate()
+        a.Input0 = True
+        a.Input1 = True
+        a.execute()
+        self.assertEqual(False, a.Output, "Class OrGate Testcase 4 failed.")
+
+class XORGateTest(unittest.TestCase):
+    def testcase_01(self):
+        a = XORGate()
         a.Input0 = False
         a.Input1 = False
         a.execute()
         self.assertEqual(False, a.Output, "Class OrGate Testcase 1 failed.")
 
     def testcase_02(self):
-        a = OrGate()
+        a = XORGate()
         a.Input0 = False
         a.Input1 = True
         a.execute()
         self.assertEqual(True, a.Output, "Class OrGate Testcase 2 failed.")
 
     def testcase_03(self):
-        a = OrGate()
+        a = XORGate()
         a.Input0 = True
         a.Input1 = False
         a.execute()
         self.assertEqual(True, a.Output, "Class OrGate Testcase 3 failed.")
 
     def testcase_04(self):
-        a = OrGate()
+        a = XORGate()
         a.Input0 = True
         a.Input1 = True
         a.execute()
-        self.assertEqual(True, a.Output, "Class OrGate Testcase 4 failed.")
+        self.assertEqual(False, a.Output, "Class OrGate Testcase 4 failed.")
 
 
 
