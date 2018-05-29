b7d6ecd537639a55a2804d4e7cab548a7ee3f5cc
SebastianB1999 <34735935+SebastianB1999@users.noreply.github.com>
2018-05-29 15:48:00 +0200

    v22

--- LogSim/LogfuncTest.py
+++ LogSim/LogfuncTest.py
@@ -11,7 +11,7 @@ class AndGateTest(unittest.TestCase):
         a = AndGate(2)
         self.assertEqual(False, a.Output, "Class AndGate Testcase 0 failed.")
         self.assertEqual(False, a.Input[0], "Class AndGate Testcase 0 failed.")
-        self.assertEqual(False, a.Input[0], "Class AndGate Testcase 0 failed.")
+        self.assertEqual(False, a.Input[1], "Class AndGate Testcase 0 failed.")
 
     def testcase_01(self):
         a = AndGate(2)
@@ -40,6 +40,45 @@ class AndGateTest(unittest.TestCase):
         a.set_input(1,True)
         a.execute()
         self.assertEqual(True, a.Output, "Class AndGate Testcase 4 failed.")
+
+    def testcase_05(self):
+        a = AndGate(3)
+        self.assertEqual(False, a.Output, "Class AndGate Testcase 5 failed.")
+        self.assertEqual(False, a.Input[0], "Class AndGate Testcase 5 failed.")
+        self.assertEqual(False, a.Input[1], "Class AndGate Testcase 5 failed.")
+        self.assertEqual(False, a.Input[2], "Class AndGate Testcase 5 failed.")
+
+    def testcase_06(self):
+        a = AndGate(3)
+        a.set_input(0,False)
+        a.set_input(1,False)
+        a.set_input(2,False)
+        a.execute()
+        self.assertEqual(False, a.Output, "Class AndGate Testcase 6 failed.")
+
+    def testcase_07(self):
+        a = AndGate(3)
+        a.set_input(0,False)
+        a.set_input(1,True)
+        a.set_input(2,False)
+        a.execute()
+        self.assertEqual(False, a.Output, "Class AndGate Testcase 7 failed.")
+
+    def testcase_08(self):
+        a = AndGate(3)
+        a.set_input(0,True)
+        a.set_input(1,False)
+        a.set_input(2,True)
+        a.execute()
+        self.assertEqual(False, a.Output, "Class AndGate Testcase 8 failed.")
+
+    def testcase_09(self):
+        a = AndGate(3)
+        a.set_input(0,True)
+        a.set_input(1,True)
+        a.set_input(2,True)
+        a.execute()
+        self.assertEqual(True, a.Output, "Class AndGate Testcase 9 failed.")
 
 
 class OrGateTest(unittest.TestCase):
@@ -77,6 +116,50 @@ class OrGateTest(unittest.TestCase):
         a.execute()
         self.assertEqual(True, a.Output, "Class OrGate Testcase 4 failed.")
 
+    def testcase_05(self):
+        a = OrGate(4)
+        self.assertEqual(False, a.Output, "Class OrGate Testcase 5 failed.")
+        self.assertEqual(False, a.Input[0], "Class OrGate Testcase 5 failed.")
+        self.assertEqual(False, a.Input[1], "Class OrGate Testcase 5 failed.")
+        self.assertEqual(False, a.Input[2], "Class OrGate Testcase 5 failed.")
+        self.assertEqual(False, a.Input[3], "Class OrGate Testcase 5 failed.")
+
+    def testcase_06(self):
+        a = OrGate(4)
+        a.set_input(0,False)
+        a.set_input(1,False)
+        a.set_input(2,False)
+        a.set_input(3,False)
+        a.execute()
+        self.assertEqual(False, a.Output, "Class OrGate Testcase 6 failed.")
+
+    def testcase_07(self):
+        a = OrGate(4)
+        a.set_input(0,False)
+        a.set_input(1,True)
+        a.set_input(2,False)
+        a.set_input(3,False)
+        a.execute()
+        self.assertEqual(True, a.Output, "Class OrGate Testcase 7 failed.")
+
+    def testcase_08(self):
+        a = OrGate(4)
+        a.set_input(0,True)
+        a.set_input(1,False)
+        a.set_input(2,True)
+        a.set_input(3,True)
+        a.execute()
+        self.assertEqual(True, a.Output, "Class OrGate Testcase 8 failed.")
+
+    def testcase_09(self):
+        a = OrGate(4)
+        a.set_input(0,True)
+        a.set_input(1,True)
+        a.set_input(2,False)
+        a.set_input(3,True)
+        a.execute()
+        self.assertEqual(True, a.Output, "Class OrGate Testcase 9 failed.")
+
 class NandGateTest(unittest.TestCase):
     def testcase_00(self):
         a = NandGate(2)
@@ -111,6 +194,50 @@ class NandGateTest(unittest.TestCase):
         a.set_input(1,True)
         a.execute()
         self.assertEqual(False, a.Output, "Class NandGate Testcase 4 failed.")
+
+    def testcase_05(self):
+        a = NandGate(4)
+        self.assertEqual(True, a.Output, "Class NandGate Testcase 5 failed.")
+        self.assertEqual(False, a.Input[0], "Class NandGate Testcase 5 failed.")
+        self.assertEqual(False, a.Input[1], "Class NandGate Testcase 5 failed.")
+        self.assertEqual(False, a.Input[2], "Class NandGate Testcase 5 failed.")
+        self.assertEqual(False, a.Input[3], "Class NandGate Testcase 5 failed.")
+
+    def testcase_06(self):
+        a = NandGate(4)
+        a.set_input(0,False)
+        a.set_input(1,False)
+        a.set_input(2,False)
+        a.set_input(3,False)
+        a.execute()
+        self.assertEqual(True, a.Output, "Class NandGate Testcase 6 failed.")
+
+    def testcase_07(self):
+        a = NandGate(4)
+        a.set_input(0,False)
+        a.set_input(1,True)
+        a.set_input(2,True)
+        a.set_input(3,False)
+        a.execute()
+        self.assertEqual(True, a.Output, "Class NandGate Testcase 7 failed.")
+
+    def testcase_08(self):
+        a = NandGate(4)
+        a.set_input(0,True)
+        a.set_input(1,False)
+        a.set_input(2,False)
+        a.set_input(3,False)
+        a.execute()
+        self.assertEqual(True, a.Output, "Class NandGate Testcase 8 failed.")
+
+    def testcase_09(self):
+        a = NandGate(4)
+        a.set_input(0,True)
+        a.set_input(1,True)
+        a.set_input(2,True)
+        a.set_input(3,True)
+        a.execute()
+        self.assertEqual(False, a.Output, "Class NandGate Testcase 9 failed.")
 
 class XORGateTest(unittest.TestCase):
     
@@ -147,6 +274,45 @@ class XORGateTest(unittest.TestCase):
         a.set_input(1,True)
         a.execute()
         self.assertEqual(False, a.Output, "Class XORGate Testcase 4 failed.")
+    
+    def testcase_05(self):
+        a = XORGate(3)
+        self.assertEqual(False, a.Output, "Class XORGate Testcase 5 failed.")
+        self.assertEqual(False, a.Input[0], "Class XORGate Testcase 5 failed.")
+        self.assertEqual(False, a.Input[1], "Class XORGate Testcase 5 failed.")
+        self.assertEqual(False, a.Input[2], "Class XORGate Testcase 5 failed.")
+
+    def testcase_06(self):
+        a = XORGate(3)
+        a.set_input(0,False)
+        a.set_input(1,False)
+        a.set_input(2,False)
+        a.execute()
+        self.assertEqual(False, a.Output, "Class XORGate Testcase 6 failed.")
+
+    def testcase_07(self):
+        a = XORGate(3)
+        a.set_input(0,False)
+        a.set_input(1,True)
+        a.set_input(2,False)
+        a.execute()
+        self.assertEqual(True, a.Output, "Class XORGate Testcase 7 failed.")
+
+    def testcase_08(self):
+        a = XORGate(3)
+        a.set_input(0,True)
+        a.set_input(1,False)
+        a.set_input(2,True)
+        a.execute()
+        self.assertEqual(True, a.Output, "Class XORGate Testcase 8 failed.")
+
+    def testcase_09(self):
+        a = XORGate(3)
+        a.set_input(0,True)
+        a.set_input(1,True)
+        a.set_input(2,True)
+        a.execute()
+        self.assertEqual(False, a.Output, "Class XORGate Testcase 9 failed.")
 
 
 
